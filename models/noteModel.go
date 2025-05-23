693d96e1b6ab91b52666713efb90194e624500b1
Yash Gupta <apple@apples-MacBook-Pro.local>
2025-05-23 14:57:43 +0530

    fix: add createdAt and updatedAt in models

--- models/noteModel.go
+++ models/noteModel.go
@@ -1,17 +1,27 @@
 package models
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type NoteModel struct {
-	NoteID  string `json:"noteId"` // Partition Key
-	UserID  string `json:"userId"` // GSI for user-wise filtering
-	Title   string `json:"title"`
-	Content string `json:"content"`
+	NoteID    string    `json:"noteId"` // Partition Key
+	UserID    string    `json:"userId"` // GSI for user-wise filtering
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
 
-func NewNote(noteId, userId, title, content string) *NoteModel {
+func NewNote(userId, title, content string) *NoteModel {
 	return &NoteModel{
-		UserID:  userId,
-		NoteID:  noteId,
-		Title:   title,
-		Content: content,
+		UserID:    userId,
+		NoteID:    uuid.New().String(),
+		Title:     title,
+		Content:   content,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
 	}
 }
